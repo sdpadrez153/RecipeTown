6b04b06f3e336b5c69bb26900457b90b7aa4b5c1
tuckerbeauchamp <tuckerbeauchamp@gmail.com>
2019-03-07 11:06:55 -0700

    add handlebars

--- public/js/init.js
+++ public/js/init.js
@@ -1,24 +1,11 @@
-(function($){
-    $(function(){
-  
-      $('.sidenav').sidenav();
-      $('.parallax').parallax();
-  
-    }); // end of document ready
-  })(jQuery); // end of jQuery name space
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    // var instances = M.Sidenav.init(elems, options);
-  });
+(function($) {
+  $(function() {
+    $(".sidenav").sidenav();
+    $(".parallax").parallax();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
 
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-
-  $(document).ready(function(){
-    $('.fixed-action-btn').floatingActionButton();
-  });
-
-  
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".sidenav");
+  // var instances = M.Sidenav.init(elems, options);
+});
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,17 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/calendar", function(req, res) {
+    res.render("calendar");
+  });
+  
+  app.get("/recipeotd", function(req, res) {
+    res.render("recipeotd");
+  });
+  
+  app.get("/contact", function(req, res) {
+    res.render("contact");
+  });
   // Load example page and pass in an example by id
   app.get("/recipe/:id", function(req, res) {
     db.Recipes.findOne({ where: { id: req.params.id } }).then(function(recipe) {
