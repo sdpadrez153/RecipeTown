c91bd9557474aea327c32647d3bfe7d28f9d1ecb
scc85 <sarahchristinec85@gmail.com>
2019-03-08 09:57:23 -0700

    update

--- public/js/index.js
+++ public/js/index.js
@@ -73,178 +73,55 @@ $.get(queryUrl)
       console.log(recipeDBinfo)
     }
   });
-
-$(document).ready(function () {
-  $(".btn").on("click", function (event) {
-    // console.log(event); 
-    $("#modal1").modal();
-    event.preventDefault();
-  });
-
-  $("#login").on("submit", function (event) {
-    event.preventDefault();
-    var newUser = {
-      user_name: $("#username").val().trim(),
-      password: $("#password").val().trim(),
-    }
-    $.ajax({
-      type: "POST",
-      url: "/api/users",
-      data: newUser
-    }).then(function (data) {
-      location.reload();
-      console.log("hi");
+  
+  $(document).ready(function () {
+    $("#sign-in").on("click", function (event) {
+      console.log("event");
+      $('#modal1').modal();
+      event.preventDefault();
+  
     });
   });
-});
 
-// // Get references to page elements
-// var $exampleText = $("#example-text");
-// var $exampleDescription = $("#example-description");
-// var $submitBtn = $("#submit");
-// var $exampleList = $("#example-list");
-
-// // The API object contains methods for each kind of request we'll make
-// var API = {
-//   saveExample: function(example) {
-//     return $.ajax({
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       type: "POST",
-//       url: "api/examples",
-//       data: JSON.stringify(example)
-//     });
-//   },
-//   getExamples: function() {
-//     return $.ajax({
-//       url: "api/examples",
-//       type: "GET"
-//     });
-//   },
-//   deleteExample: function(id) {
-//     return $.ajax({
-//       url: "api/examples/" + id,
-//       type: "DELETE"
-//     });
-//   }
-// };
-
-// // refreshExamples gets new examples from the db and repopulates the list
-// var refreshExamples = function() {
-//   API.getExamples().then(function(data) {
-//     var $examples = data.map(function(example) {
-//       var $a = $("<a>")
-//         .text(example.text)
-//         .attr("href", "/example/" + example.id);
-
-//       var $li = $("<li>")
-//         .attr({
-//           class: "list-group-item",
-//           "data-id": example.id
-//         })
-//         .append($a);
-
-//       var $button = $("<button>")
-//         .addClass("btn btn-danger float-right delete")
-//         .text("ｘ");
-
-//       $li.append($button);
-
-//       return $li;
-//     });
-
-//     $exampleList.empty();
-//     $exampleList.append($examples);
-//   });
-// };
-
-// // handleFormSubmit is called whenever we submit a new example
-// // Save the new example to the db and refresh the list
-// var handleFormSubmit = function(event) {
-//   event.preventDefault();
-
-//   var example = {
-//     text: $exampleText.val().trim(),
-//     description: $exampleDescription.val().trim()
-//   };
-
-//   if (!(example.text && example.description)) {
-//     alert("You must enter an example text and description!");
-//     return;
-//   }
-
-//   API.saveExample(example).then(function() {
-//     refreshExamples();
-//   });
-
-//   $exampleText.val("");
-//   $exampleDescription.val("");
-// };
-
-// // handleDeleteBtnClick is called when an example's delete button is clicked
-// // Remove the example from the db and refresh the list
-// var handleDeleteBtnClick = function() {
-//   var idToDelete = $(this)
-//     .parent()
-//     .attr("data-id");
-
-//   API.deleteExample(idToDelete).then(function() {
-//     refreshExamples();
-//   });
-// };
-
-// // Add event listeners to the submit and delete buttons
-// $submitBtn.on("click", handleFormSubmit);
-// $exampleList.on("click", ".delete", handleDeleteBtnClick);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 524c516143ed5b744aceb6f288fc36266d454176
-
-$(document).ready(function () {
-  $(".btn").on("click", function (event) {
-    console.log("event");
-    $('#modal1').modal();
-    event.preventDefault();
-
-  });
-  $("#new_user").on("submit", function (event) {
+  $(document).on("click", "#modalSubmit", function (event) {
     event.preventDefault();
     console.log("newUser")
-    // var newUser = {
-    //     user_name: $("#username").val().trim(),
-    // };
+    var newUser = {
+        user_name: $("#username").val().trim(),
+        // eventually ecrypt passwords
+    };
 
-    // $.ajax("/api/recipes", {
-    //     type: "POST",
-    //     url: "/api/recipes",
-    //     data: newUser
-    // }).then(function (data) {
-    //     location.reload();
-    //     console.log(newUser);
-    // });
+    $.ajax("/api/users", {
+        type: "POST",
+        url: "/api/users",
+        data: newUser
+    }).then(function (data) {
+        location.reload();
+    });
   });
-});
 
-$("#new_user").on("submit", function (event) {
-  // event.preventDefault();
-  console.log("newUser")
-  // var newUser = {
-  //     user_name: $("#username").val().trim(),
-  // };
-
-  // $.ajax("/api/recipes", {
-  //     type: "POST",
-  //     url: "/api/recipes",
-  //     data: newUser
-  // }).then(function (data) {
-  //     location.reload();
-  //     console.log(newUser);
+  // $(document).ready(function () {
+  //   $(".btn").on("click", function (event) {
+  //     // console.log(event); 
+  //     $("#modal1").modal();
+  //     event.preventDefault();
+  //   });
+  
+  //   $("#login").on("submit", function (event) {
+  //     event.preventDefault();
+  //     var newUser = {
+  //       user_name: $("#username").val().trim(),
+  //       password: $("#password").val().trim(),
+  //     }
+  //     $.ajax({
+  //       type: "POST",
+  //       url: "/api/users",
+  //       data: newUser
+  //     }).then(function (data) {
+  //       location.reload();
+  //       console.log("hi");
+  //     });
+  //   });
   // });
-});
-<<<<<<< HEAD
->>>>>>> c5ef7f11762bddb2342b2a9d07dd42f735a59746
-=======
->>>>>>> 524c516143ed5b744aceb6f288fc36266d454176
+
+
